4aea227c1bb626161d2643b68b5608462acf37e1
prashanthk02 <prashanth.k369@gmail.com>
2022-07-22 19:27:43 -0400

    configure new route

--- backend/routes/new.js
+++ backend/routes/new.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('new list item');
-});
+//helper functions
+const addItem = function(productName, openedOn, useBy, user_id, db) {
+  const queryString = `INSERT INTO list(user_id, productName, openedOn, useBy)
+    VALUES ($1, $2, $3, $4) RETURNING *`;
 
-module.exports = router;
+    return db.query(queryString, [user_id, productName, openedOn, useBy])
+      .then((result) => {
+        return result.rows[0];
+      });
+};
+
+module.exports = (db) => {
+  router.get('/', (req, res) => {
+    return db
+      .query("SELECT * FROM list")
+      .then((result) => {
+        return res.json(result.rows);
+      })
+  });
+
+  router.post('/', (req, res) => {
+    const item = req.body;
+
+    addItem(list.user_id, list.productName, list.openedOn, list.useBy, db)
+    .then(result => {
+      return res.json({ message: "Added" });
+    })
+    .catch(err => {
+      res.json({ error: err.message });
+    });
+  })
+
+  return router;
+}
